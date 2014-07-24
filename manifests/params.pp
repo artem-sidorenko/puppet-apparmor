a19b50188cc888a951e373ab8507fd8d254b2b29
Artem Sidorenko <artem@2realities.com>
2014-07-24 11:03:03 +0200

    added manifests skels

---
+++ manifests/params.pp
@@ -0,0 +1,24 @@
+#
+# apparmor puppet module
+# https://github.com/artem-sidorenko/puppet-apparmor
+#
+# Author Artem Sidorenko artem@2realities.com
+#
+# Copyright 2014 Artem Sidorenko and contributors.
+# Copyright 2014 Deutsche Telekom AG
+# See the COPYRIGHT file at the top-level directory of this distribution
+# and at https://github.com/artem-sidorenko/puppet-apparmor/blob/master/COPYRIGHT
+#
+class apparmor::params(
+){
+
+  case $::osfamily {
+    'Debian': {
+      $package_names = ['apparmor']
+      $service_name  = 'apparmor'
+    }
+    default: {
+      fail("Unsupported osfamily (${::osfamily})")
+    }
+  }
+}
---
+++ manifests/profile.pp
@@ -0,0 +1,14 @@
+#
+# apparmor puppet module
+# https://github.com/artem-sidorenko/puppet-apparmor
+#
+# Author Artem Sidorenko artem@2realities.com
+#
+# Copyright 2014 Artem Sidorenko and contributors.
+# Copyright 2014 Deutsche Telekom AG
+# See the COPYRIGHT file at the top-level directory of this distribution
+# and at https://github.com/artem-sidorenko/puppet-apparmor/blob/master/COPYRIGHT
+#
+define apparmor::profile(
+){
+}
